f63be85de6c187a826db8ce3cb1a9d5c15d157ae
bonsa <bonsa>
2024-06-06 09:38:24 +0300

    module css

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,20 @@
 import { Link } from "react-router-dom";
+import style from "./navbar.module.css";
 
 function NavBar() {
   return (
-    <nav className="navbar">
-      <Link to="/home_page">Home</Link>
-      <Link to="/pricing"> pricing</Link>
-      <Link to="/product"> product</Link>
+    <nav className={style.nav}>
+      <ul>
+        <li>
+          <Link to="/home_page">Home</Link>
+        </li>
+        <li>
+          <Link to="/pricing"> pricing</Link>
+        </li>
+        <li>
+          <Link to="/product"> product</Link>
+        </li>
+      </ul>
     </nav>
   );
 }
